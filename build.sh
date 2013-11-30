4956ed65fbadaeda1786dcc936ed72a47632d992
Darcy Brás da Silva <dardevelin@cidadecool.com>
2013-11-30 03:00:49 +0000

    add stm.h boilerplate

---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc -std=c99 `pkg-config --cflags --libs gtk+-3.0`  stm.c stm_ui.c -o stm
+
---
+++ stm.h
@@ -0,0 +1,26 @@
+/*
+ * STM-GTK - Stereomood Gtk
+ * Copyright (C) 2013 Darcy Brás da Silva
+ *
+ * stm.h is part of STM-GTK.
+ *
+ * STM-GTK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STM-GTK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with STM-GTK.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef STM_H
+#define STM_H
+
+
+#endif /* stm.h | STM_H */
