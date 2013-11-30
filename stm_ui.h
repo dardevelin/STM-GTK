77b5d2381ed66423f67f3b778eb4f85b8c73b6f1
Darcy Brás da Silva <dardevelin@cidadecool.com>
2013-11-30 03:00:15 +0000

    add stm_ui.h and boilerplate

---
+++ stm_ui.h
@@ -0,0 +1,37 @@
+/*
+ * STM-GTK - Stereomood Gtk
+ * Copyright (C) 2013 Darcy Brás da Silva
+ *
+ * stm_ui.h is part of STM-GTK.
+ *
+ * STM-GTK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STM-GTK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with STM-GTK.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef STM_UI_H
+#define STM_UI_H
+
+#define STM_MW_WIDTH 450
+#define STM_MW_HEIGHT 450
+
+/*text data*/
+#define STM_MW_TITLE "STM | STEREOMOOD GTK"
+#define STM_BTN_SEARCH_STR "Search"
+#define STM_BTN_DOWNLOAD_STR "Download"
+#define STM_BTN_TOGGLE_PLAY_STR_0 "Play"
+#define STM_BTN_TOGGLE_PLAY_STR_1 "Pause"
+#define STM_TXT_ENTRY_SEARCH_TOOLTIP_STR "Type your mood here eg: calm"
+
+
+#endif /* stm_ui.h | STM_UI_H */
