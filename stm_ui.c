dbec13030492d6fe74599c5375ea8ce2d9c81a34
Darcy Brás da Silva <dardevelin@cidadecool.com>
2013-11-30 03:58:50 +0000

    create stm_ui_init and fix typo stm_ui_init initializes all base widgets and checks them

--- stm_ui.c
+++ stm_ui.c
@@ -20,7 +20,7 @@
  */
 
 #include "stm_ui.h"
-
+#include <gtk/gtk.h>
 
 static GtkWidget *main_window;
 static GtkWidget *txt_entry_search;
@@ -30,7 +30,7 @@ static GtkWidget *btn_toggle_play;
 
 static GtkWidget *main_grid;
 static GtkWidget *top_grid;
-static GtkWidgte *middle_grid;
+static GtkWidget *middle_grid;
 static GtkWidget *bottom_grid;
 
 static GtkWidget *playlist_treeview;
@@ -41,8 +41,34 @@ static GtkTreeViewColumn *column;
 static GtkWidget *list;
 static GtkListStore *store;
 
-/* creates all widgets with base definitions*/
+/* creates all base widgets */
 static int stm_ui_init()
 {
+	main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+	/* create the grids that hold the control widgets */
+	main_grid = gtk_grid_new();
+	middle_grid = gtk_grid_new();
+	bottom_grid = gtk_grid_new();
+	
+	txt_entry_search = gtk_entry_new();
+	btn_search = gtk_button_new();
+	btn_download = gtk_button_new();
+	btn_toggle_play = gtk_button_new();
+
+	/* necessary evil. lets check if everything was created */
+	if(!main_window) return -1;
+
+	if(!main_grid)   return -2;
+	if(!middle_grid) return -3;
+	if(!bottom_grid) return -4;
+
+	if(!txt_entry_search) return -5;
+	if(!btn_search)       return -6;
+	if(!btn_download)     return -7;
+	if(!btn_toggle_play)  return -8;
+
 	return 1;
-}
+
+}/* end stm_ui_init() */
+
