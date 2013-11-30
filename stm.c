c797cbf06e5ebd294a7b491b77612e3c4c463842
Darcy Brás da Silva <dardevelin@cidadecool.com>
2013-11-30 02:59:27 +0000

    add stm.c boilerplate

---
+++ stm.c
@@ -0,0 +1,28 @@
+/*
+ * STM-GTK - Stereomood Gtk
+ * Copyright (C) 2013 Darcy Brás da Silva
+ *
+ * stm.c is part of STM-GTK.
+ *
+ * STM-GTK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STM-GTK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with STM-GTK.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "stm.h"
+#include "stm_ui.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
